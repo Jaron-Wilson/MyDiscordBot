0e52dfe2e7f535996f50f0c5bafa8f158d074210
Jaron-Wilson <JaronWilson2024@gmail.com>
2021-11-10 15:42:30 -0500

    Updated

--- src/main/java/me/jaron/test/messages/Messages.java
+++ src/main/java/me/jaron/test/messages/Messages.java
@@ -2,6 +2,7 @@ package me.jaron.test.messages;
 
 import me.jaron.test.Main;
 import net.dv8tion.jda.api.entities.Emote;
+import net.dv8tion.jda.api.entities.MessageReaction;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -9,16 +10,24 @@ import javax.annotation.Nonnull;
 
 public class Messages extends ListenerAdapter {
 
-    String messages = "Message!";
+    String messages = "hi";
     String emojiuni = "U+1F47E";
 
     public void onGuildMessageReceived(@Nonnull GuildMessageReceivedEvent event) {
         String messageSent = event.getMessage().getContentRaw();
 
+            event.getMessage().addReaction(emojiuni).queue();
         event.getMessage().addReaction("😝").queue();
+        event.getMessage().addReaction("🗡").queue();
+
+        if (messageSent.equalsIgnoreCase(Main.prefix + "clone")) {
+            event.getChannel().createCopy().queue();
+            return;
+        }
         if (messageSent.equalsIgnoreCase(Main.prefix + messages)) {
-//            event.getMessage().addReaction(emoji1).queue();
-            event.getMessage().getReactionByUnicode(emojiuni);
+            event.getMessage().addReaction("🤔").queue();
+            event.getMessage().addReaction("🗡").queue();
+            return;
         }
     }
 
